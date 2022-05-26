7f2c535a0a9181443498da797150531c63711d99
tylerstamperrr <tylergstamper@gmail.com>
2022-05-25 23:03:11 -0700

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/public/images`,
+        path: `${__dirname}/static/images`,
       },
     },
     {
