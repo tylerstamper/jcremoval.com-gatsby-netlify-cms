04ef5001ba2be790b85fda66f30e0e601bf06aa8
tylerstamperrr <tylergstamper@gmail.com>
2022-05-26 00:31:36 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -128,7 +128,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `public/assetts/logo-outline-white.png`, // This path is relative to the root of the site.
+        icon: `static/assetts/logo-outline-white.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
